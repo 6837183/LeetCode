7562fc886e7ac6b537d5e9a2070ff828f1de35cd
6837183 <2501520585@qq.com>
2020-08-03 15:23:30 +0800

    a

---
+++ 875/main.go
@@ -0,0 +1,14 @@
+package main	//包声明
+	// go 是一个天生支撑多核计算的云开发时代c语言
+	//node + go 服务器有关
+	//require('fs')fileSystem
+	import (
+		"fmt" //  向命令行输出hello world	format
+	)//es6
+	// node 脚本语言
+	// go c 二进制文件
+	// 编译原理器
+func main()  {	// 函数关键字
+	// 从main开始
+	fmt.Println("hello world")
+}
